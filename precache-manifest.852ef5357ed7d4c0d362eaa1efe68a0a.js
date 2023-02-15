00284afa987255ee5b28bdb7716e4ece689a3111
enitchals <ellen.nitchals@gmail.com>
2023-02-14 22:54:03 -0800

    Updates

---
+++ precache-manifest.852ef5357ed7d4c0d362eaa1efe68a0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c47c9c2fc2f1a7a8c3f6",
+    "url": "/snake/static/css/main.c7624bd7.chunk.css"
+  },
+  {
+    "revision": "c47c9c2fc2f1a7a8c3f6",
+    "url": "/snake/static/js/main.c47c9c2f.chunk.js"
+  },
+  {
+    "revision": "c0655a5f557061888341",
+    "url": "/snake/static/js/1.c0655a5f.chunk.js"
+  },
+  {
+    "revision": "da1ecade61dd97107e27",
+    "url": "/snake/static/js/runtime~main.da1ecade.js"
+  },
+  {
+    "revision": "1beb49d3b9c73ff6f4331e9965c34470",
+    "url": "/snake/index.html"
+  }
+];
